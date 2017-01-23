f6598e5134ca9b94ccb77350f1555477b2c604ce
david <ostermann.david@gmail.com>
2017-01-23 15:20:39 +0100

    add files

---
+++ program.js
@@ -0,0 +1,8 @@
+var robot = require('./robot.js');
+
+var helloStr = robot.sayHello('michel');
+
+var noStr = robot.sayNo('michel');
+
+console.log(helloStr);
+console.log(noStr);
---
+++ robot.js
@@ -0,0 +1,12 @@
+var robot = {
+
+  sayHello: function(prenom) {
+    return 'hello '+ prenom;
+  },
+  sayNo: function(prenom) {
+    return 'No, '+ prenom;
+  }
+
+};
+
+module.exports = robot;
